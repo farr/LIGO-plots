fe385e8b48d966967f6d048b154642a0e208b7a7
Will M. Farr <will.farr@ligo.org>
2022-10-04 15:12:17 -0400

    Scriptified.

---
+++ GW150914-ptemcee-script/README.md
@@ -0,0 +1,67 @@
+# Example of running `ptemcee`-based Bilby sampling.
+
+To run GW150914 following the official Bilby example, you can issue 
+
+```shell
+python /path/to/LIGO-plots/main/scripts/bilby_ptemcee --ini config.ini
+```
+
+in the current directory.  Output will appear in `outdir`, including checkpoint
+files (`checkpoint.nc`) and various status plots as the run progresses:
+
+- `beta.png` shows the evolution of the inverse temperatures of the parallel
+  chains versus iteration number as they adapt to the posterior surface.
+- `mean-likelihood.png` shows the mean of the likelihood (actually posterior,
+  but who's keeping track?) for the `T = 1` (cold) chain's ensemble versus
+  iteration number.  Early on, this will be increasing, as the ensemble moves to
+  high-likelihood positions; later on, this will fluctuate randomly, as the
+  ensemble explores the high-likelihood part of parameter space.
+- `trace.png` shows a traceplot of the evolution of each walker in each
+  parameter over each iteration.  Once the walkers start to explore the
+  high-likelihood region of parameter space, and the total number of iterations
+  (always 128*`thin` with `thin` increasing until the sampling converges)
+  increases, this will get smoother and smoother.
+- `flat-trace.png` shows a "flattened" trace where all walkers have been jammed
+  together into a single chain; this ultimately reflects the posterior density.
+
+The run will go through a number of phases (sometimes reverting to earlier
+phases if it detects the necessity):
+- Initially there is a short interval between checkpoints as the walkers move to
+  the high-likelihood places in parameter space.  Short runs will be repeated as
+  long as the average likelihood keeps increasing.
+- Once the high-likelihood region is found, walkers start exploring.  In this
+  phase, we run for longer and longer stretches between checkpoints /
+  convergence tests, doubling `thin` and the total number of MCMC steps at each
+  iteartion (so the number of *saved* steps remains constant).  Eventually
+  either a new high-likelihood region is found (in which case we revert to the
+  initial phase again), or the autocorrelation length calculation shows that the
+  chains have converged (in which case we stop the run).  We print the estimated
+  autocorrelation length in each parameter at the end of each iteration in this
+  phase; convergence occurs when the chains reach 50 autocorrelation lengths in
+  each parameter.
+- Finally we output the chains (sampled-over parameters plus various computed
+  parameters) to `posterior.nc` in `arviz`-readable format (see
+  [here](https://python.arviz.org/en/latest/)) for subsequent processing.
+
+By default, the sampler will try to start first from the checkpointed position
+of a previous run in `{outdir}/checkpoint.nc` (you can disable by setting
+`try_checkpoint:False` in ini file); if you want a fresh run, you need to delete
+this file or specify a new output directory.  Note: this is not a *true*
+checkpoint---the run starts over---but just using the stored positions in
+`checkpoint.nc` to initialize the sampler!
+
+By default, the sampler will use the coordinate systems from my special branch
+of Bilby, at https://git.ligo.org/will-farr/bilby/-/tree/spherical-coordinates;
+you will need to install Bilby from that branch, or else disable the spherical
+topology (leading to less-efficient `ptemcee` sampling, alas!) by setting
+`spherical_topology:False` in the ini file).
+
+Set the `pool_size` option in the ini file to the number of cores you want to
+use.  Each step in the evolution of all the walkers involves `nwalkers*ntemps`
+likelihood calls (the default settings have `64*8 = 512` likelihood calls per
+step).  As long as there are several likelihood calls per core (i.e. `pool_size
+<< nwalkers*ntemps`), there is minimal communication overhead, so the speedup
+should be linear in the number of cores.
+
+For the full list of `ini` file options, see the beginning of
+`scripts/bilby_ptemcee.py`.
---
+++ scripts/bilby_ptemcee.py
@@ -0,0 +1,328 @@
+import argparse
+import arviz as az
+import bilby
+import configparser
+import emcee
+from gwpy.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+import multiprocessing
+import numpy as np
+import os.path as op
+import pandas as pd
+import ptemcee
+import sys
+from tqdm import tqdm
+import xarray as xr
+
+# Needed to make the multiprocessing work on my MacBook
+if sys.platform.startswith('darwin'):
+    multiprocessing.set_start_method('fork')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='program to run bilby with ptemcee')
+    parser.add_argument('--ini', default='config.ini')
+    
+    args = parser.parse_args()
+
+    cp = configparser.ConfigParser()
+    cp.read(args.ini)
+    cp = cp['DEFAULT']
+
+    logger = bilby.core.utils.logger
+    outdir = cp.get('outdir', 'outdir')
+    label = cp.get('label', '')
+
+    # Set to the number of cores you want to run on.
+    pool_size = cp.getint('pool_size', 8)
+
+    nw = cp.getint('nwalkers', 64)
+    nt = cp.getint('ntemps', 8)
+
+    try_checkpoint = cp.getboolean('try_checkpoint', True)
+
+    trigger_time = cp.getfloat('trigger_time')
+    
+    detector_string = cp.get('detectors', 'H1,L1')
+    detectors = detector_string.split(',')
+
+    maximum_frequency = cp.getfloat('maximum_frequency', 512)
+    minimum_frequency = cp.getfloat('minimum_frequency', 20)
+
+    # Duration of the Tukey window in s.
+    roll_off = cp.getfloat('rolloff', 0.4)
+
+    # Duration of the analysis segment, s.
+    duration = cp.getfloat('duration', 4)
+
+    # Part of analysis segment after the trigger.
+    post_trigger_duration = cp.getfloat('post_trigger_duration', 2)
+
+    end_time = trigger_time + post_trigger_duration
+    start_time = end_time - duration
+
+    npsd = cp.getint('num_psd_segments', 32)
+    psd_duration = npsd*duration
+
+    psd_start_time = cp.getfloat('psd_start_time', start_time-psd_duration)
+    psd_end_time = psd_start_time + psd_duration
+
+    prior_file = cp.get('prior_file', 'GW150914.prior')
+
+    # Use the standard Bilby coodinate system, which will make sampling very
+    # inefficient.
+    topology = cp.getboolean('spherical_topology', True)
+
+    approximant = cp.get('approximant', 'IMRPhenomXPHM')
+    fref = cp.getfloat('fref', 50)
+
+    time_marginalization = cp.getboolean('time_marginalization', True)
+    phase_marginalization = cp.getboolean('phase_marginalization', False)
+    distance_marginalization = cp.getboolean('distance_marginalization', True)
+
+    ifo_list = bilby.gw.detector.InterferometerList([])
+    for det in detectors:
+        logger.info("Downloading analysis data for ifo {}".format(det))
+        ifo = bilby.gw.detector.get_empty_interferometer(det)
+        data = TimeSeries.fetch_open_data(det, start_time, end_time, cache=True)
+        ifo.strain_data.set_from_gwpy_timeseries(data)
+
+        logger.info("Downloading psd data for ifo {}".format(det))
+        psd_data = TimeSeries.fetch_open_data(det, psd_start_time, psd_end_time, cache=True)
+        psd_alpha = 2 * roll_off / duration
+        psd = psd_data.psd(
+            fftlength=duration, overlap=0, window=("tukey", psd_alpha), method="median"
+        )
+        ifo.power_spectral_density = bilby.gw.detector.PowerSpectralDensity(
+            frequency_array=psd.frequencies.value, psd_array=psd.value
+        )
+        ifo.maximum_frequency = maximum_frequency
+        ifo.minimum_frequency = minimum_frequency
+        ifo_list.append(ifo)
+
+    logger.info("Saving data plots to {}".format(outdir))
+    bilby.core.utils.check_directory_exists_and_if_not_mkdir(outdir)
+    ifo_list.plot_data(outdir=outdir, label=label)
+
+    priors = bilby.gw.prior.BBHPriorDict(filename=prior_file)
+
+    # Add in the new coordinates---for this part you will need my branch from
+    # https://git.ligo.org/will-farr/bilby/-/tree/spherical-coordinates You
+    # *probably* don't really need it, but it will help with the acceptance rate
+    # and convergence.  You can turn this off by setting `spherical_topology` to
+    # `False` in the ini
+    if topology:
+        del priors['ra']
+        del priors['dec']
+        priors['sky_x'] = bilby.core.prior.Normal(0,1,name='sky_x')
+        priors['sky_y'] = bilby.core.prior.Normal(0,1,name='sky_y')
+        priors['sky_z'] = bilby.core.prior.Normal(0,1,name='sky_z')
+
+        del priors['phase']
+        priors['phase_x'] = bilby.core.prior.Normal(0,1,name='phase_x')
+        priors['phase_y'] = bilby.core.prior.Normal(0,1,name='phase_y')
+
+        del priors['theta_jn']
+        del priors['psi']
+        priors['rad_x'] = bilby.core.prior.Normal(0,1,name='rad_x')
+        priors['rad_y'] = bilby.core.prior.Normal(0,1,name='rad_y')
+        priors['rad_z'] = bilby.core.prior.Normal(0,1,name='rad_z')
+
+        del priors['phi_12']
+        priors['phi_12_x'] = bilby.core.prior.Normal(0,1,name='phi_12_x')
+        priors['phi_12_y'] = bilby.core.prior.Normal(0,1,name='phi_12_y')
+
+        del priors['phi_jl']
+        priors['phi_jl_x'] = bilby.core.prior.Normal(0,1,name='phi_jl_x')
+        priors['phi_jl_y'] = bilby.core.prior.Normal(0,1,name='phi_jl_y')
+
+    priors["geocent_time"] = bilby.core.prior.Uniform(
+        trigger_time - 0.1, trigger_time + 0.1, name="geocent_time"
+    )
+
+    waveform_generator = bilby.gw.WaveformGenerator(
+        frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
+        parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
+        waveform_arguments={
+            "waveform_approximant": approximant,
+            "reference_frequency": fref,
+    })
+
+    likelihood = bilby.gw.likelihood.GravitationalWaveTransient(
+        ifo_list,
+        waveform_generator,
+        priors=priors,
+        time_marginalization=time_marginalization,
+        phase_marginalization=phase_marginalization,
+        distance_marginalization=distance_marginalization)
+
+    # Don't ask me why, but this fails the first time through when
+    # marginalization is turned on, but the second time through the marginalized
+    # parameters will be set up 
+    try:
+        sampler = bilby.sampler.Ptemcee(likelihood, priors, outdir=outdir, label=label, 
+                                        nwalkers=nw, ntemps=nt, 
+                                        adapt=True, adaptation_time=10, adaptation_lag=100, Tmax=np.inf,
+                                        plot=True, Q_tol=1.001, store_walkers=True)
+    except KeyError:
+        sampler = bilby.sampler.Ptemcee(likelihood, priors, outdir=outdir, label=label, 
+                                        nwalkers=nw, ntemps=nt, 
+                                        adapt=True, adaptation_time=10, adaptation_lag=100, Tmax=np.inf,
+                                        plot=True, Q_tol=1.001, store_walkers=True)
+
+    ptsampler = sampler.setup_sampler()
+    sampler_kws = sampler.sampler_function_kwargs
+
+    cfile = op.join(outdir, 'checkpoint.nc')
+    nd = len(sampler.search_parameter_keys)
+    if try_checkpoint and op.exists(cfile):
+        pdata = az.from_netcdf(cfile)
+        p0 = np.zeros((nt, nw, nd))
+        for i, k in enumerate(sampler.search_parameter_keys):
+            x = pdata.posterior[k].values[:,-1]
+            p0[:,:,i] = np.tile(x, (nt, 1))
+        pos0 = p0
+        print('setup from checkpoint')
+    else:
+        pos0 = sampler.pos0
+
+    # Setup for the sampling loop
+    pos = pos0
+    nt, nw, nd = pos.shape
+    niter = 128 # If we need at least 50 ACTs to measure the correlation time, this ensures that the ACT will be <~ 4 samples after thinning at convergence.
+    thin = 1
+    max_mean_log_like = np.NINF
+    converged = False
+
+    def logprior(x):
+        params = {k: t for k,t in zip(sampler.search_parameter_keys, x)}
+        return sampler.priors.ln_prob(params)
+    def loglike(x):
+        params = {k: t for k,t in zip(sampler.search_parameter_keys, x)}
+        likelihood.parameters.update(params)
+        return likelihood.log_likelihood() - likelihood.noise_log_likelihood()
+
+    global likelihood_fn
+    likelihood_fn = loglike
+    global prior_fn
+    prior_fn = logprior
+
+    class LikePriorEvaluator(object):
+        def __init__(self):
+            pass
+        def __call__(self, x):
+            lp = prior_fn(x)
+            if lp == np.NINF:
+                return np.NINF, np.NINF
+            else:
+                ll = likelihood_fn(x)
+                return ll, lp
+
+    with multiprocessing.Pool(processes=pool_size) as p:
+        ptsampler.pool = p
+        ptsampler._likeprior = LikePriorEvaluator()
+        while not converged:
+            ptsampler.reset()
+            for pos, log_post, log_like in tqdm(ptsampler.sample(pos, storechain=True, iterations=niter*thin, thin=thin, **sampler_kws), total=niter*thin):
+                pass
+
+            lps = ptsampler.logprobability[0,-1,:]
+            mean_log_like = np.mean(lps)
+            se_log_like = np.std(lps)/np.sqrt(len(lps))
+            if mean_log_like > max_mean_log_like + 3*se_log_like:
+                print('resetting sampler due to significant log-likelihood increase')
+                print(f'current mean log(post) = {mean_log_like:.3f} (+/- {se_log_like:.3f})')
+                print('is significantly larger than')
+                print(f'previous best mean log(post) = {max_mean_log_like:.3f}')
+
+                # Reset the sampler
+                thin = 1
+                max_mean_log_like = mean_log_like
+
+                # # Cluster around the high-likelihood point
+                # cov = np.cov(pos[0,:,:], rowvar=False)
+                # bw = cov/1e2 # 10 times smaller
+                # pbest = pos[np.unravel_index(np.argmax(log_like), log_like.shape) + (None,)]
+
+                # pos = np.random.multivariate_normal(pbest.squeeze(), bw, size=log_like.shape)
+                # pos[0,0,:] = pbest.squeeze() # Ensure that the best point ends up in the sampler.
+            else:
+                thin = 2*thin
+
+            print('Mean acceptance fraction:')
+            print(np.mean(ptsampler.acceptance_fraction, axis=1))
+            print()
+
+            print('Mean tswap fraction:')
+            print(ptsampler.tswap_acceptance_fraction)
+
+            with open(op.join(outdir, 'stats.txt'), 'w') as f:
+                f.write('Mean acceptance fraction:\n{}\n'.format(np.mean(ptsampler.acceptance_fraction, axis=1)))
+                f.write('Mean tswap fraction:\n{}\n'.format(ptsampler.tswap_acceptance_fraction))
+
+            nt, nw, ns, nd = ptsampler.chain.shape
+
+            plt.figure()
+            plt.plot(np.arange(ns), np.mean(ptsampler.logprobability[0,:,:], axis=0))
+            plt.xlabel(r'Iteration')
+            plt.ylabel(r'$\left\langle \log \pi \right\rangle_{\beta = 1}$')
+            plt.savefig(op.join(outdir, 'mean-likelihood.png'))
+
+            plt.figure()
+            plt.plot(ptsampler.beta_history.T)
+            plt.xlabel(r'Iteration')
+            plt.ylabel(r'$\beta$')
+            plt.savefig(op.join(outdir, 'beta.png'))
+
+            idata = az.convert_to_inference_data({k: ptsampler.chain[0, :, :, i] for (i,k) in enumerate(sampler.search_parameter_keys)})
+            az.to_netcdf(idata, op.join(outdir, 'checkpoint.nc'))
+
+            az.plot_trace(idata)
+            plt.savefig(op.join(outdir, 'trace.png'))
+
+            nt, nw, ns, nd = ptsampler.chain.shape
+            flat_chain = ptsampler.chain.reshape((nt, 1, nw*ns, nd))
+            flat_idata = az.convert_to_inference_data({k:flat_chain[0,:,:,i] for (i,k) in enumerate(sampler.search_parameter_keys)})
+            az.plot_trace(flat_idata)
+            plt.savefig(op.join(outdir, 'flat-trace.png'))
+
+            # The chain is (Ntemp, Nwalker, Nstep, Ndim), so we take the cold chain with
+            # `[0,...]` then average over the number of walkers, then put in a "singleton"
+            # walker dimension for emcee, which wants (Nstep, Nwalker, Ndim) inputs.
+            try:
+                avg_chain = np.mean(ptsampler.chain[0,...], axis=0)[:, None, :]
+                taus = emcee.autocorr.integrated_time(avg_chain)
+            except emcee.autocorr.AutocorrError as er:
+                print('Looping with bad autocorrelation: ')
+                print(er)
+                continue
+            converged = True
+            break
+
+
+    chain = {}
+    for k in sampler.search_parameter_keys:
+        chain[k] = idata.posterior[k].stack(dim=('chain', 'draw')).values
+    chain_df = pd.DataFrame(chain, columns=sampler.search_parameter_keys)
+
+    filled_chain = bilby.gw.conversion.generate_all_bbh_parameters(chain_df, likelihood=likelihood, priors=priors)
+
+    nc = len(idata.posterior.chain)
+    nd = len(idata.posterior.draw)
+
+    cc = idata.posterior.chain.data
+    dc = idata.posterior.draw.data
+
+
+    for k, v in filled_chain.items():
+        vv = v.values.reshape((nc, nd))
+        idata.posterior[k] = (('chain', 'draw'), vv)
+
+    # These are complex numbers and/or arbitrary Python objects, which NetCDF
+    # won't store.  Sigh.
+    del idata.posterior['waveform_approximant']
+    del idata.posterior['H1_matched_filter_snr']
+    del idata.posterior['L1_matched_filter_snr']
+
+    az.to_netcdf(idata, op.join(outdir, 'posterior.nc'))
+
+    
